6342a9769fd3384af3648cf628425c2e8f1abcd6
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-11-14 17:39:41 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+string[] array = new string[6] {"hello", "cat", "2022", ":-)", "Russia", "13"};
+string[] resultArray = FillResultArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", resultArray)}]");
+
+int CountElementsLessThreeChar(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] FillResultArray(string[] array)
+{
+    string[] resultArray = new string[CountElementsLessThreeChar(array)];
+    for(int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
